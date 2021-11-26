e84762234b8ec1e99504c702afd301679f8db9fc
Josh Rogers <josh.rogers@catchco.com>
2021-11-26 16:28:52 -0600

    initial commit

---
+++ src/Jens.Store.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Jens.Store.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = "Obi";
+            var currentTime = DateTime.Now;
+
+            if (currentTime < DateTime.Parse("12:00:00"))
+            {
+                Console.WriteLine($"Good Morning {username}!");
+            }
+            else if (currentTime < DateTime.Parse("17:00:00"))
+            {
+                Console.WriteLine($"Good Afternoon {username}!");
+            }
+            else
+            {
+                Console.WriteLine($"Good Evening {username}!");
+            }
+        }
+    }
+}
